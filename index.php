323621fa09cc0a9f255a327d61458f438cc58dbf
mazvydasko <mazvydasko@gmail.com>
2019-08-28 00:11:25 +0300

    Finished

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comments</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
