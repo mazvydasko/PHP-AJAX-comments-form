0cc4eea923ee9b792fc5c5ed126f6c08553ec1fa
mazvydasko <mazvydasko@gmail.com>
2019-08-29 12:13:07 +0300

    pdo update

--- functions.php
+++ functions.php
@@ -1,49 +1,76 @@
 <?php
 
-// connect to database
-$db = mysqli_connect("localhost", "root", "", "comments-reply");
+$db["db_user"] = "root";
+$db["db_pass"] = "";
+
+foreach ($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=comments-reply", DB_USER, DB_PASS);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
 
 // Get all comments from database
-$comments_query_result = mysqli_query($db, "SELECT * FROM comments ORDER BY created_at DESC");
-$comments = mysqli_fetch_all($comments_query_result, MYSQLI_ASSOC);
+$stmt = $conn->prepare("SELECT * FROM comments ORDER BY created_at DESC");
+$stmt->execute();
+$comments = $stmt->fetchAll();
 
 // Receives a comment id and returns the username
 function getRepliesByCommentId($id)
 {
-    global $db;
-    $result = mysqli_query($db, "SELECT * FROM replies WHERE comment_id=$id");
-    $replies = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    global $conn;
+    $stmt =  $conn->prepare("SELECT * FROM replies WHERE comment_id=$id");
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $replies = $stmt -> fetchAll();
     return $replies;
 }
 
 function getCommentsCount()
 {
-    global $db;
-    $resultComments = mysqli_query($db, "SELECT id FROM comments");
-    $resultReplies = mysqli_query($db, "SELECT id FROM replies");
-    $numComments = $resultComments->num_rows;
-    $numReplies = $resultReplies->num_rows;
+    global $conn;
+    $resultCommentsStm = $conn->prepare( "SELECT id FROM comments");
+    $resultCommentsStm->execute();
+    $resultRepliesStm = $conn->prepare( "SELECT id FROM replies");
+    $resultRepliesStm->execute();
+    $numComments = $resultCommentsStm->rowCount();
+    $numReplies = $resultRepliesStm->rowCount();
     $result = $numComments + $numReplies;
     return $result;
 }
 
 if (isset($_POST['comment_posted'])) {
-    global $db;
-    $comment_text = $_POST['comment_text'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    global $conn;
+    $comment_text = (string) htmlspecialchars($_POST['comment_text']);
+    $name = (string) htmlspecialchars($_POST['name']);
+    $email = (string) htmlspecialchars($_POST['email']);
 
-    $sql = "INSERT INTO comments (body, name, email) VALUES ('$comment_text', '$name', '$email')";
-    $result = mysqli_query($db, $sql);
+    $sql = "INSERT INTO comments (body, name, email) VALUES (?,?,?)";
+    $stmt= $conn->prepare($sql);
+    $result = $stmt->execute([$comment_text, $name, $email]);
 
     // Query same comment from database to send back to be displayed
-    $inserted_id = $db->insert_id;
-    $res = mysqli_query($db, "SELECT * FROM comments WHERE id=$inserted_id");
-    $inserted_comment = mysqli_fetch_assoc($res);
+    $inserted_id = $conn->lastInsertId(); ;
+    $stmt = $conn->prepare( "SELECT * FROM comments WHERE id=?");
+    $stmt->execute([$inserted_id]);
+//    $inserted_comment = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // if insert was successful, get that same comment from the database and return it
+
+//
+//
+//    // if insert was successful, get that same comment from the database and return it
     if ($result) {
-        $comment = "<div class='comment clearfix'>
+        while ($inserted_comment = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $comment = "<div class='comment clearfix'>
 					    <div class='comment-details'>
 						    <span class='comment-name'>" . $inserted_comment['name'] . "</span>
 						    <span class='comment-date'>" . $inserted_comment['created_at'] . "</span>
@@ -71,11 +98,13 @@ if (isset($_POST['comment_posted'])) {
                         </form>
                         <div class='replies_wrapper_" . $inserted_comment['id'] . "'></div>
 				        </div>";
-        $comment_info = array(
-            'comment' => $comment,
-        );
-        echo json_encode($comment_info);
-        exit();
+            $comment_info = array(
+                'comment' => $comment,
+            );
+            echo json_encode($comment_info);
+            exit();
+        }
+
     } else {
         echo "error";
         exit();
@@ -84,30 +113,34 @@ if (isset($_POST['comment_posted'])) {
 
 // If the user clicked submit on reply form...
 if (isset($_POST['reply_posted'])) {
-    global $db;
-    $reply_text = $_POST['reply_text'];
-    $comment_id = $_POST['comment_id'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    global $conn;
+    $reply_text = (string) htmlspecialchars($_POST['reply_text']);
+    $comment_id = (string) htmlspecialchars($_POST['comment_id']);
+    $name = (string) htmlspecialchars($_POST['name']);
+    $email = (string) htmlspecialchars($_POST['email']);
 
-    $sql = "INSERT INTO replies (comment_id, body, name, email) VALUES ('$comment_id', '$reply_text', '$name', '$email')";
-    $result = mysqli_query($db, $sql);
+    $sql = "INSERT INTO replies (comment_id, body, name, email) VALUES (?,?,?,?)";
+    $stmt= $conn->prepare($sql);
+    $result = $stmt->execute([$comment_id,$reply_text, $name, $email]);
 
-    $inserted_id = $db->insert_id;
-    $res = mysqli_query($db, "SELECT * FROM replies WHERE id=$inserted_id");
-    $inserted_reply = mysqli_fetch_assoc($res);
+    $inserted_id = $conn->lastInsertId(); ;
+    $stmt = $conn->prepare( "SELECT * FROM replies WHERE id=?");
+    $stmt->execute([$inserted_id]);
 
     // if insert was successful, get that same reply from the database and return it
     if ($result) {
-        $reply = "<div class='comment reply clearfix'>
+        while ($inserted_reply = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $reply = "<div class='comment reply clearfix'>
 					<div class='comment-details'>
 						<span class='comment-name'>" . $inserted_reply['name'] . "</span>
 						<span class='comment-date'>" . $inserted_reply['created_at'] . "</span>
 						<p>" . $inserted_reply['body'] . "</p>
 					</div>
 				</div>";
-        echo $reply;
-        exit();
+            echo $reply;
+            exit();
+        }
+
     } else {
         echo "error";
         exit();
